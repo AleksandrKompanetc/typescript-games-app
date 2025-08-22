4b7f76ee7f26da3020a135a6de3a61fde09c4d75
AleksandrKompanetc <moneymacker7@gmail.com>
2025-08-22 15:41:54 +0300

    creating app page

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isAdult, setIsAdult] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [games, setGames] = useState([]);
+
+  useState(() => {
+    getGamesFromServer().then((gamesFromServer) => {
+      setLoading(false);
+      setGames(gamesFromServer);
+    });
+  }, []);
+
+  const renderTags = () => {
+    return tags?.map((tag, index) => (
+      <span key={tag + index}>{tag}</span>
+    ))
+  }
+
+  if (loading) {
+    return (
+      <div>Loading...</div>
+    )
+  }
+
+  if (isAdult === null) {
+    return (
+      <AdultOrNot setIsAdult={setIsAdult} />
+    )
+  }
 
   return (
-    <>
-      VITE
-    </>
+    <div>
+      {games.map((game) => (
+        <div key={game.id} style={{border: '1px solid blue'}}>
+          Game:
+          {game.forKids && !isAdult ? (
+            <b style={{color: 'red'}}></b>
+          ) : (
+            <>
+              <Game 
+                name={game.name}
+                description={game.description}
+              />
+            </>
+          )}
+        </div>
+      ))}
+    </div>
   )
 }
 
