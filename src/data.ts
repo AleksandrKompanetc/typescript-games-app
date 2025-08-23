239e34595101bae0e6809c7fed5ab542f3bdd670
AleksandrKompanetc <moneymacker7@gmail.com>
2025-08-23 23:36:48 +0300

    added Games file

---
+++ src/data.ts
@@ -0,0 +1,24 @@
+export const GAMES = [
+  {
+    id: 1,
+    name: 'Mortal Kombat',
+    description: 'A fighting game franchise created by Midway Games.',
+    version: 'X',
+    price: 1000,
+    tags: ['fighting', 'arcade', 'multiplayer'],
+    forKids: false,
+  },
+  {
+    id: 2,
+    name: 'Racing',
+    price: 700,
+    tags: ['racing', 'arcade', 'multiplayer'],
+    forKids: true,
+  },
+  {
+    id: 3,
+    name: 'FIFA',
+    description: 'A football simulation video game developed by EA Sports.',
+    forKids: true,
+  }
+]
