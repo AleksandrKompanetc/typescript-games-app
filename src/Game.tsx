f75e34d6e78f734f89a3719ddac6e493cc579253
AleksandrKompanetc <moneymacker7@gmail.com>
2025-08-26 19:06:58 +0300

    created component Game

---
+++ src/Game.tsx
@@ -0,0 +1,11 @@
+function Game({ name, description, version}) {
+  return (
+    <div>
+      <p>Name: {name}</p>
+      <p>Description: {description}</p>
+      <p>Version: {version}</p>
+    </div>
+  )
+}
+
+export default Game;
